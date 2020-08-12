43af7819ec9627229f096f2e3b99a3837ea6ff07
Aman Gaur <gaman@athenahealth.com>
2020-08-12 23:45:33 +0530

    Adding to-use API's

--- placement_group.py
+++ placement_group.py
@@ -1,7 +1,24 @@
 import boto3
 
 ec2_client = boto3.client('ec2')
-response = ec2_client.describe_placement_groups()
+sts_client = boto3.client('sts')
+# Assuming the user has AccessKeyId or any access to be honest,
+# we can retrieve the account details.
+# 1. Get the account details.
+# 2. Get regions for the connected user, c2.describe_regions()
+#   and availability zones.
+# 3. Iterate through the regions and describe placement groups.
 
+caller_identity = sts_client.get_caller_identity()
+active_regions = ec2_client.describe_regions(
+    Filters=[{
+        'Name': 'opt-in-status',
+        'Values': ['opted-in']
+    }]
+)
+
+active_availability_zones = ec2_client.describe_availability_zones()
+
+response = ec2_client.describe_placement_groups()
 for group in response['PlacementGroups']:
     print(group['GroupName'])
