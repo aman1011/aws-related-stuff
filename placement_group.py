d6882d6bf5a253b34d3d6e82ec874f5a8898cccc
Aman Gaur <gaman@athenahealth.com>
2020-08-14 09:20:33 +0530

    adding try except

--- placement_group.py
+++ placement_group.py
@@ -53,7 +53,10 @@ for account in aws_accounts:
     regions = get_regions_for_stlb_account()
     for region in regions:
         print("Establishing a session for account " + account + " and region " + region)
-        session = create_session(region, account)
+        try:
+            session = create_session(region, account)
+        except Exception as e:
+            print(e)
         ec2_client = session.client("ec2")
         response = ec2_client.describe_placement_groups()
         print_placement_group(response)
